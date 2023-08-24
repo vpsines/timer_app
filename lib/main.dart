b26675830d64a0c9349d4ada7052ab0a20e64d04
vyshnav.sasidharan <sasip6028@gmail.com>
2023-08-24 16:29:18 +0530

    feat: add data provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:timer_app/providers/data_provider.dart';
+import 'package:timer_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,25 +16,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: ChangeNotifierProvider(
+        create: (context) => DataProvider(),
+        child: const HomeScreen(),
+      ),
     );
   }
 }
--- lib/providers/data_provider.dart
+++ lib/providers/data_provider.dart
@@ -1,3 +1,66 @@
-class DataProvider{
-  
+import 'package:flutter/material.dart';
+import 'package:timer_app/models/item.dart';
+
+class DataProvider extends ChangeNotifier {
+  List<Item> items = <Item>[];
+
+  String message = "";
+
+  TextEditingController minutesController = TextEditingController();
+  TextEditingController secondsController = TextEditingController();
+
+  void addTimer(int minutes, int seconds) {
+    Item newItem = Item(minutes, seconds);
+    newItem.startTimer();
+    items.add(newItem);
+    notifyListeners();
+  }
+
+  void validateInput() {
+    message = "";
+    notifyListeners();
+
+    if (minutesController.text.isEmpty) {
+      message = "* Minutes required.";
+      notifyListeners();
+    } else if (secondsController.text.isEmpty) {
+      message = "* Seconds required.";
+      notifyListeners();
+    } else {
+      int min = int.tryParse(minutesController.text) ?? 0;
+      if (min >= 59) {
+        message = "* Minute value cannot be 59 or more.";
+        notifyListeners();
+        return;
+      }
+
+      int sec = int.tryParse(secondsController.text) ?? 0;
+      if (sec > 60) {
+        message = "* Seconds value cannot be 59 or more.";
+        notifyListeners();
+        return;
+      }
+
+      if(min == 0 && sec == 0){
+         message = "* Please add atleast seconds value.";
+        notifyListeners();
+        return;
+      }
+
+      int activeTimers = items.where((item) => item.isActive).length;
+      if (activeTimers < 10) {
+        addTimer(min, sec);
+      } else {
+        message = "* Only 10 timers allowed at a time.";
+        notifyListeners();
+      }
+    }
+  }
+
+  void saveTimer() {}
+
+  void resetControllers() {
+    minutesController.clear();
+    secondsController.clear();
+  }
 }
